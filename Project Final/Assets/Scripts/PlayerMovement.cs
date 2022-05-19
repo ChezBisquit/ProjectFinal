4af070d488c36995d698607cf99fcef0dcae63b4
Carrillo <adalskywalker@icloud.com>
2022-05-19 09:21:19 -0700

    There is Player Movement
    Things added:
    - Scripts
    - Jonsey
    -Tree
    -Cube
    -Sprites

---
+++ Project Final/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+    public AudioClip hitSound;
+    bool isInvincible;
+    float invincibleTimer;
+    public AudioClip throwSound;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    AudioSource audioSource;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+      
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+
+       
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+    }
+
+} 
